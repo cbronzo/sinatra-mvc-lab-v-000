94f2572fb3b59fd7a8d9b7013614512446c00ca3
Catherine Bronzo <cbronzo@mac.com>
2018-08-10 21:27:09 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,14 +6,14 @@ class PigLatinizer
     split_text = text.split(" ")
     split_text.each do |word|
       if "AEIOUaeiou".include?(word[0])
-        results << "#{word}way"
+        results.push("#{word}way")
       else
         n=0
         while !"AEIOUaeios".include?(word[n])
           n+=1
         end
         
-        results << "#{word[n..-1]+word[0..n-1]}ay"
+        results.push("#{word[n..-1]+word[0..n-1]}ay")
       end
     end
     results.join(" ").strip
